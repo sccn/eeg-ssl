14c16b60c2ce8c142419bbba594dd59519677920
Arnaud Delorme <arnodelorme@gmail.com>
2025-01-10 11:07:44 -0800

    adding new files and moving the RelativePositioningDataset to ssl_utils

--- libs/ssl_utils.py
+++ libs/ssl_utils.py
@@ -8,7 +8,32 @@ from pstats import SortKey, Stats
 from libs import eeg_utils
 import os
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from braindecode.datasets import BaseConcatDataset
 
+class RelativePositioningDataset(BaseConcatDataset):
+    """BaseConcatDataset with __getitem__ that expects 2 indices and a target.
+    """
+
+    def __init__(self, list_of_ds):
+        super().__init__(list_of_ds)
+        self.return_pair = True
+
+    def __getitem__(self, index):
+        if self.return_pair:
+            ind1, ind2, y = index
+            return (super().__getitem__(ind1)[0],
+                    super().__getitem__(ind2)[0]), y
+        else:
+            return super().__getitem__(index)
+
+    @property
+    def return_pair(self):
+        return self._return_pair
+
+    @return_pair.setter
+    def return_pair(self, value):
+        self._return_pair = value
+        
 class MaskedContrastiveLearningTask():
     def __init__(self,
                 dataset: torch.utils.data.Dataset,
