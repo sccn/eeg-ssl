4020ac6baf31ef3e5d1dbf3f82e5ca3b58b93d4c
Arnaud Delorme <arnodelorme@gmail.com>
2025-01-13 11:41:58 -0800

    fix class

--- libs/ssl_utils.py
+++ libs/ssl_utils.py
@@ -6,33 +6,6 @@ from torch.utils.data import DataLoader, random_split
 import os
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from braindecode.datasets import BaseConcatDataset
-<<<<<<< HEAD
-=======
-
-class RelativePositioningDataset(BaseConcatDataset):
-    """BaseConcatDataset with __getitem__ that expects 2 indices and a target.
-    """
-
-    def __init__(self, list_of_ds):
-        super().__init__(list_of_ds)
-        self.return_pair = True
-
-    def __getitem__(self, index):
-        if self.return_pair:
-            ind1, ind2, y = index
-            return (super().__getitem__(ind1)[0],
-                    super().__getitem__(ind2)[0]), y
-        else:
-            return super().__getitem__(index)
-
-    @property
-    def return_pair(self):
-        return self._return_pair
-
-    @return_pair.setter
-    def return_pair(self, value):
-        self._return_pair = value
->>>>>>> 496838cbdfc1608c7ea57ee0cbc76f86cbc084ea
 
 class RelativePositioningDataset(BaseConcatDataset):
     """BaseConcatDataset with __getitem__ that expects 2 indices and a target.
