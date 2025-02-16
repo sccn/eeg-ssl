cf3d6bf63d42d2cddc50feaacc325a6ad8b31972
Dung Truong <dt.young112@gmail.com>
2025-02-16 09:10:17 -0800

    example litserve

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from libs.ssl_model import LitSSL, VGGSSL
 from lightning.pytorch.cli import LightningCLI
 
 def main():
-    cli = LightningCLI(LitSSL, RelativePositioningHBNDataModule, save_config_callback=None, subclass_mode_model=True)
+    cli = LightningCLI(LitSSL, RelativePositioningHBNDataModule, subclass_mode_model=True)
 
 if __name__ == '__main__':
     main()
